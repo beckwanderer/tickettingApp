0dc5014f35df6dbf875db43dd566bda603397be6
beckwanderer <beck_wanderer@hotmail.com>
2022-07-18 15:51:01 +1000

    files got corrupted resending

--- index.js
+++ index.js
@@ -131,10 +131,13 @@ let checkContactDetails = () => {
 }
 
 
+/*
+Manage the tab
+*/
 function openTab(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {ls
+    for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
